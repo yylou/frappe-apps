ae9a58865b3308dc87781ab0cd458844479c39bd
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-08 10:23:47 +0000

    Add WebForm & Doctype

---
+++ frappe_apps/web_form/doctype/web_form_data/web_form_data.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Yuan-Yao Lou and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Web Form Data', {
+	// refresh: function(frm) {
+
+	// }
+});
