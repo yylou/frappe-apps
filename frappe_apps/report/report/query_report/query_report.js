4a8d7c0587d85a592367a707d5079383097e29cf
Yuan-Yao Lou <yylou0701@gmail.com>
2021-06-09 08:33:17 +0000

    Add Report: Query Report

---
+++ frappe_apps/report/report/query_report/query_report.js
@@ -0,0 +1,32 @@
+/**
+ *	@Author	:	Yuan-Yao Lou (Mike) <lou45@purdue.edu | yuanyao.lou@gmail.com>
+ *	@Title	:	PhD in ECE at Purdue University
+ *	@Date	:	2021/06/09
+ */
+
+ 
+ frappe.query_reports['Qu Report'] = {
+    "filters": [
+        {
+            'fieldname':    'from_date',
+            'label':        __( 'From' ),
+            'fieldtype':    'Date',
+	        'default':      '2021-01-01',
+            'reqd':         1
+        },
+        {
+            'fieldname':    'to_date',
+            'label':        __( 'To' ),
+            'fieldtype':    'Date',
+	        'default':      get_today(),
+            'reqd':         1
+        },
+        {
+            'fieldname':    'first_name',
+            'label':        __( 'First Name' ),
+            'fieldtype':    'Data',
+	        'default':      '%',
+            'reqd':         1
+        },
+    ]
+}
